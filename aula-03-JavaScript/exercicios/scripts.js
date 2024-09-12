4301f32aa9484bfb024fc2d0877c2bce53709799
Gabriel <gfelixfaustina@gmail.com>
2024-09-12 10:57:14 -0300

    Exercício: Alunos

--- aula-03-JavaScript/exercicios/scripts.js
+++ aula-03-JavaScript/exercicios/scripts.js
@@ -4,35 +4,32 @@ const quantidadeAlunos = prompt("Quantos alunos tem na sala?");
 const listaAlunos = [];
 
 // Funções
-var mediaAluno = (notaTotal, quantidadeNotas) => {
-    return notaTotal / quantidadeNotas;
-}
-
-var validaNota = (mediaAluno) => {
-    return mediaAluno >= 7 ? true : false;
-}
-
 function somaNotas(quantidadeNotas) {
     let notaTotal = 0;
     for (let index = 0; index < quantidadeNotas; index++) {
-        notaTotal += Number(prompt(`Digite a Nota ${index + 1}`));
+        notaTotal += Number(prompt(`Nota ${index + 1}: `));
     }
-    return mediaAluno(notaTotal, quantidadeNotas);
+    return notaTotal / quantidadeNotas;
 }
 
 for (let index = 0; index < quantidadeAlunos; index++) {
 
+    let nomeAluno = prompt(`Nome do Aluno ${index + 1}: `)
     let quantidadeNotas = Number(prompt(`Quantidade de notas do aluno ${index + 1}: `));
+    let mediaCalculada = somaNotas(quantidadeNotas); 
 
     let aluno = {
         id: index,
-        nome: prompt("Nome do Aluno: "), 
-        mediaAluno: somaNotas(quantidadeNotas),
-        aprovacao: validaNota(mediaAluno)
+        nome: nomeAluno, 
+        mediaAluno: mediaCalculada,
+        aprovacao: mediaCalculada >= 7 ? true : false
     }
 
-    listaAlunos.push(aluno);
-    console.log(listaAlunos);
-    console.log(`APROVADOS: \n ${listaAlunos.filter(aprovacao => aprovacao = true).map(aluno => `Nome: ${aluno.nome}\nMédia: ${aluno.mediaAluno}`)}`);
-    console.log(`REPROVADOS: \n ${listaAlunos.filter(aprovacao => aprovacao = false).map(aluno => `Nome: ${aluno.nome}\nMédia: ${aluno.mediaAluno}`)}`);
+    listaAlunos.push(aluno);  
 }
+const listaAprovados = listaAlunos.filter(aluno => aluno.aprovacao);
+const listaReprovados = listaAlunos.filter(aluno => !aluno.aprovacao);
+
+console.log(listaAlunos);
+console.log(listaAprovados);
+console.log(listaReprovados);
