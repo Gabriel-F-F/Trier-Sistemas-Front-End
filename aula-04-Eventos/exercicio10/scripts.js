f9767094473fdcc2dd9739194c80baa2427ed5f9
Gabriel <gfelixfaustina@gmail.com>
2024-09-12 23:33:54 -0300

    zHorckz

--- aula-04-Eventos/exercicio10/scripts.js
+++ aula-04-Eventos/exercicio10/scripts.js
@@ -1,21 +1,75 @@
-const listaTarefas = [{id: 0, todo: "Ir na Padaria!", status: false, excluir: false}];
-console.log(contador());
-
-function tabela(lista) {
-    const tabela = document.getElementById("tabela");
-    const tr = document.createElement(`<tr> <td>${listaTarefas.id}</td> <td>${listaTarefas.todo}</td><td>${listaTarefas.status}</td><td>${listaTarefas.excluir}</td> <tr>`);
-
-    listaTarefas.forEach(id => {
-        lista = document.append()
-    });
-}
+let listaTarefas = [];
 
 function adicionarTarefa() {
 
-    let tarefa = {
-        todo: todo,
-        status: false
+    const input = document.getElementById("input-adicionar");
+
+    if (input.value === "") {
+        return alert("Deve-se digitar uma tarefa!");
     }
 
+    let tarefa = {
+        id: contador(),
+        todo: input.value,
+        status: false,
+        excluido: false
+    }
     listaTarefas.push(tarefa);
+    input.value = "";
+    atualizarTabela();
+}
+
+function contador() {
+    let contador = 0;
+    listaTarefas.forEach(tarefa => {
+        contador++;
+    });
+    contador++;
+    return contador;
+}
+
+function atualizarTabela() {
+
+    const tabela = document.getElementById("tabela");
+    tabela.innerHTML = "";
+
+    listaTarefas.forEach(tarefa => {
+        const linha = document.createElement("tr");
+
+        const colunaId = document.createElement("td");
+        colunaId.textContent = tarefa.id;
+        linha.appendChild(colunaId);
+
+        const colunaTarefa = document.createElement("td");
+        colunaTarefa.textContent = tarefa.todo;
+        linha.appendChild(colunaTarefa);
+
+        const colunaStatus = document.createElement("td");
+        const checkboxStatus = document.createElement("input");
+        checkboxStatus.type = "checkbox";
+        checkboxStatus.checked = tarefa.status;
+        checkboxStatus.addEventListener("change", () => {
+            tarefa.status = checkboxStatus.checked();
+        });
+        colunaStatus.appendChild(checkboxStatus);
+        linha.appendChild(colunaStatus);
+
+        const colunaExcluir = document.createElement("td");
+        const botaoExcluir = document.createElement("button");
+        botaoExcluir.typeContent = "Excluir";
+        botaoExcluir.style = "height: 20px; width: 150px;"
+        botaoExcluir.checked = tarefa.excluido;
+        botaoExcluir.addEventListener("click", () => {
+            excluirTarefa(tarefa.id);
+        });
+        colunaExcluir.appendChild(botaoExcluir);
+        linha.appendChild(colunaExcluir);
+
+        tabela.appendChild(linha);
+    });
+}
+
+function excluirTarefa(id) {
+    listaTarefas = listaTarefas.filter(tarefa => tarefa.id !== id);
+    atualizarTabela();
 }
