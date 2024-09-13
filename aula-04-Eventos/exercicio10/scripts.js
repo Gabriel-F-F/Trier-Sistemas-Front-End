18abce54a40d69beb9ff7025cfeb57b5f1a0da68
Gabriel <gfelixfaustina@gmail.com>
2024-09-12 21:53:54 -0300

    commit

--- aula-04-Eventos/exercicio10/scripts.js
+++ aula-04-Eventos/exercicio10/scripts.js
@@ -1,11 +1,9 @@
-const listaTarefas = [{ todo: "Ir na Padaria", checked: false },
-                    { todo: "Passear com a Girafa", checked: false },
-                    { todo: "Voar de Helicóptero", checked: false }];
+const listaTarefas = [{id: 0, todo: "Ir na Padaria!", status: false, excluir: false}];
 console.log(contador());
 
 function tabela(lista) {
     const tabela = document.getElementById("tabela");
-    const tr = document.createElement("<tr> <td>1</td> <td>1</td><td>1</td><td>1</td> <tr>");
+    const tr = document.createElement(`<tr> <td>${listaTarefas.id}</td> <td>${listaTarefas.todo}</td><td>${listaTarefas.status}</td><td>${listaTarefas.excluir}</td> <tr>`);
 
     listaTarefas.forEach(id => {
         lista = document.append()
@@ -16,7 +14,7 @@ function adicionarTarefa() {
 
     let tarefa = {
         todo: todo,
-        checked: false
+        status: false
     }
 
     listaTarefas.push(tarefa);
